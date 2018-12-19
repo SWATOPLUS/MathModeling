4b15965f20d892e07a826e6522db3ad12ac6cf07
Pavel Zhdanovich <swatoplus@gmail.com>
2018-12-19 12:33:46 +0300

    Implemented monte carlo integral calculator

--- Lab3MonteCarlo/Program.cs
+++ Lab3MonteCarlo/Program.cs
@@ -1,43 +1,49 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
+using MathModeling.MonteCarlo;
 
 namespace Lab3MonteCarlo
 {
     class Program
     {
-        static void Main(string[] args)
+        static Program()
         {
-            var exactResult = 0.567544;
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-us");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-us");
+        }
 
-            var n = 30001;
-            var a = new double[n];
-            var az = new double[n];
-            var m = 15.0;
-            var betta = Math.Pow(2.0, m) + 3.0;
-            var M = (int)Math.Pow(2, m - 1);
+        private static void Main()
+        {
+            var realValue = 1.3463879568;
 
-            az[0] = betta;
-            var a1 = 0.0;
-            Console.WriteLine($"betta = {betta}");
-            for (var t = 0; t < n - 1; t++)
+            var repeats = 1;
+
+            var sampleSizes = new[]
             {
-                az[t + 1] = (betta * az[t]) % M;
-                a[t + 1] = az[t + 1] / M;
-                /*Console.WriteLine("a[");
-                Console.WriteLine(t);
-                Console.WriteLine("]= ");
-                Console.WriteLine(a[t]);*/
-            }
-            for (var j = 1; j < n; j++)
-            {
-                a[j - 1] = a[j - 1] * 3;
-                a1 = a1 + Math.Exp(-1 * a[j - 1]) * Math.Cos(a[j - 1]) * Math.Cos(a[j - 1]);
-            }
-            a1 = 3 * a1 / n;
+                10, 20, 50,
+                100, 200, 500,
+                1000, 2000, 5000,
+                10000, 20000, 50000,
+                100000, 200000, 500000,
+                1000000, 2000000, 5000000
+            };
 
-            Console.WriteLine($"Точное решение = {exactResult}");
-            Console.WriteLine("Решение методом Монте Карло = " + a1);
-            Console.WriteLine("Погрешность = " + Math.Abs(a1 - exactResult));
-            Console.ReadLine();
+            var calculator = new MonteCarloIntegralCalculator(-1, 3, x => Math.Exp(-x * x) * Math.Cos(x));
+
+            Console.WriteLine($"Real value {realValue}");
+            Console.WriteLine("N\t  result\terror");
+
+            foreach (var sampleSize in sampleSizes)
+            {
+                foreach (var _ in Enumerable.Range(0, repeats))
+                {
+                    var integral = calculator.CalculateIntegral(sampleSize);
+                    var error = Math.Abs(integral - realValue);
+                    Console.WriteLine($"{sampleSize.ToString().PadRight(10)}{integral:E}\t{error:E}");
+                }
+            }
+
         }
     }
 }
---
+++ MathModeling/MonteCarlo/MonteCarloIntegralCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MathModeling.MonteCarlo
+{
+    public class MonteCarloIntegralCalculator
+    {
+        private static readonly Random Random = new Random(0);
+
+        public double From { get; }
+
+        public double To { get; }
+
+        public Func<double, double> IntegralFunction { get; }
+
+        public MonteCarloIntegralCalculator(double from, double to, Func<double, double> integralFunction)
+        {
+            From = from;
+            To = to;
+            IntegralFunction = integralFunction;
+        }
+
+        private static IEnumerable<double> GetUniformRandomValues(int n, double a, double b)
+        {
+            return Enumerable.Range(0, n)
+                .Select(x => Random.NextDouble())
+                .Select(x => a + (b - a) * x);
+        }
+
+        public double CalculateIntegral(int n)
+        {
+            var values = GetUniformRandomValues(n, From, To)
+                .Select(IntegralFunction);
+
+            return (To - From) * values.Average();
+        }
+    }
+}
